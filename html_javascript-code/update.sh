6109d3e81e4ee0e0946f548f7527b2ce430b04aa
DerAnderDo <andrea.notaro@protonmail.ch>
2022-09-19 23:31:40 +0200

    Implemented Lowpass

--- html_javascript-code/update.sh
+++ html_javascript-code/update.sh
@@ -1,1 +1,1 @@
-scp /home/andi/Desktop/temp_project/html/index.lighttpd.html root@192.168.0.213:/var/www/index.lighttpd.html
+scp /home/andi/Desktop/temp_project/github/Rock64_CpuTempOverBrowser/html_javascript-code/index.lighttpd.html root@192.168.0.213:/var/www/index.lighttpd.html
